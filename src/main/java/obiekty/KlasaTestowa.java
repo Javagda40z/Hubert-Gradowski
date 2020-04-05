310078ab265057e371b55a102fc53d56f46636c9
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-04-05 12:12:37 +0200

    Dodanie do zadania na interface wariantu dla listy

--- src/main/java/obiekty/KlasaTestowa.java
+++ src/main/java/obiekty/KlasaTestowa.java
@@ -1,30 +1,43 @@
 package obiekty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KlasaTestowa {
 
     public static void main(String[] args) {
 
         Animal[] zwierzeta = new Animal[3];
+        List<Animal> listaZwierzat = new ArrayList<>();
 
-//        Cat kot = new Cat();
-//        Dog pies = new Dog();
-//        Cow krowa = new Cow();
-
-//        zwierzeta[0] = kot;
-//        zwierzeta[1] = pies;
-//        zwierzeta[2] = krowa;
-
-        //Na potrzeby zadania wystarczy w ten sposob:
         zwierzeta[0] = new Cat();
         zwierzeta[1] = new Dog();
         zwierzeta[2] = new Cow();
 
-        for (Animal zwierze : zwierzeta) {
+        listaZwierzat.add(new Cat());
+        listaZwierzat.add(new Dog());
+        listaZwierzat.add(new Cow());
+        listaZwierzat.add(new Cow());
+        listaZwierzat.add(new Cow());
 
+        dajGlos(zwierzeta);
+        System.out.println();
+        dajGlos(listaZwierzat);
+
+    }
+
+    public static void dajGlos(Animal[] zwierzeta){
+
+        for (Animal zwierze : zwierzeta) {
+            zwierze.makeSound();
+        }
+    }
+
+    public static void dajGlos(List<Animal> zwierzeta){
+        for (Animal zwierze : zwierzeta) {
             zwierze.makeSound();
 
         }
-
     }
 
 }
